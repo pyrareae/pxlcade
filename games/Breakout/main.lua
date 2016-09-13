b277d80b23cf6ef7f4091579611663f0c7fd72d6
Jake Vandereay <gvandereay@gmail.com>
2016-09-12 23:27:36 -0700

    brick gen/draw and collision detection

--- games/Breakout/main.lua
+++ games/Breakout/main.lua
@@ -1,8 +1,59 @@
 local timer = require "timer"
 local M = {
 name = "Breakout",
-c = {}
+c = {},
+bricks = {}
 }
+local function boxcoll(x,y,xb,yb,x2,y2,x2b,y2b)--box collision check http://gamedev.stackexchange.com/questions/29786/a-simple-2d-rectangle-collision-algorithm-that-also-determines-which-sides-that
+    local w = 0.5*((xb-x)+(x2b-x2))
+    local h = 0.5*((yb-y)+(y2b-y2))
+    local dx = x+(xb-x)/2 - x2+(x2b-x2)/2
+    local dy = y+(yb-y)/2 - y2+(y2b-y2)/2
+
+    if (math.abs(dx) <= w and math.abs(dy) <= h) then
+        local wy = w * dy
+        local hx = h * dx
+
+        if (wy > hx) then
+            if (wy > -hx) then
+                return 'top'
+            else
+                return 'left'
+            end
+        else
+            if (wy > -hx) then
+                return 'right'
+            else
+                return 'bottom'
+            end
+        end
+    end
+    return nil
+end
+local Brick = {
+    new = function(self, color, x, y, xb, yb)
+        o = {}
+        setmetatable(o, self)
+        o.color = color
+        o.pos = {x=x,y=y,xb=xb,yb=yb,length=xb-x+1,height=yb-y+1}
+        return o
+    end
+}
+Brick.__index = Brick
+function M:buildbricks() 
+    local width = 8
+    local height = 4
+    local stack = 4
+    local hue = math.random(2, 7)
+    self.bricks = {}
+    for y=1, stack do
+        self.bricks[y] = {}
+        local curr = self.bricks[y]
+        for x=1, width do
+            curr[x] = Brick:new(self.PXL.colors[hue][math.random(2,4)], (x-1)*width, (y-1)*height, x*width-1, y*height-1)
+        end
+    end
+end
 local function inc(num, amount, a, b, rollback)--smart incrementer
     local num = num + amount
     if not a and not b then
@@ -39,8 +90,8 @@ function M:load()
         reset = function(this)
             this.x = self.screen.x/2
             this.y = self.screen.y*0.75
---             this.vel = {x = math.random(-30,30), y = 30}
-            this.vel = {x = 0, y = 30}
+            this.vel = {x = math.random(-20,20), y = 20}
+--             this.vel = {x = 10, y = 10}
         end,
         radius = 1,
         getx = function(self) return math.floor(self.x+self.radius) end,--return center coord
@@ -59,11 +110,37 @@ function M:load()
                 this:move(dt)
             end
             --check for paddle
-            if this:gety()+this.radius >= self.player.y and this:getx()+this.radius >= self.player.x and this:getx()-this.radius < self.player.x+self.player.size.x then
-                print('this.y: '..this.y..', player.y: '..self.player.y)
+            if this:gety()+this.radius+1 >= self.player.y and this:getx()+this.radius >= self.player.x and this:getx()-this.radius < self.player.x+self.player.size.x then
                 this.y = self.player.y-(this.radius*2+1)
                 this.vel.y = -this.vel.y
                 this:move(dt)
+                self.c.ball = self.c.paddle
+            end
+            --check for bricks
+            for i, row in ipairs(self.bricks) do
+                for i2, brick in ipairs(row) do
+                    local side = nil
+                    if brick then side = boxcoll(brick.pos.x, brick.pos.y, brick.pos.xb, brick.pos.yb, this.x, this.y, this.x+2*this.radius, this.y+2*this.radius) end
+                    if side then
+                        print(side)
+                        if side == "bottom" then
+                            this.vel.y = - this.vel.y
+--                             this.y = brick.pos.yb+1
+                        elseif side == "top" then
+                            this.vel.y = - this.vel.y
+--                             this.y = brick.pos.y - (2*this.radius+1)
+                        elseif side == "right" then
+                            this.vel.x = - this.vel.x
+--                             this.x = brick.pos.x + (2*this.radius+1)
+                        elseif side == "left" then
+                            this.vel.x = - this.vel.x
+--                             this.x = brick.pos.xb+1
+                        end
+                        this:move(dt, true)
+                        self.c.ball = brick.color
+                        table.remove(self.bricks[i], i2)
+                    end
+                end
             end
         end,
         move = function(this, dt, nocheck)
@@ -75,6 +152,7 @@ function M:load()
         end
     }
     self.ball:reset()
+    self:buildbricks()
 end
 function M:mousemoved(x,y,dx,dy,touch)
     
@@ -102,6 +180,22 @@ function M:draw()
     --draw paddle
     love.graphics.setColor(self.c.paddle)
     love.graphics.rectangle('line', self.player.x, self.player.y, self.player.size.x, self.player.size.y)
+    --draw bricks
+    for _, row in ipairs(self.bricks) do
+        for _, brick in ipairs(row) do
+            love.graphics.setColor(brick.color)
+            love.graphics.rectangle('fill', brick.pos.x, brick.pos.y, brick.pos.length, brick.pos.height)
+            love.graphics.setLineWidth(1)
+            love.graphics.setColor({255,255,255,100})
+            love.graphics.line(brick.pos.xb+1, brick.pos.y+1, 
+                                brick.pos.x+1, brick.pos.y+1,
+                                brick.pos.x+1, brick.pos.yb+1)--shadow/highlights
+            love.graphics.setColor({0,0,0,100})
+            love.graphics.line(brick.pos.xb+1, brick.pos.y+1,
+                                brick.pos.xb+1, brick.pos.yb+1,
+                                brick.pos.x+1, brick.pos.yb+1)
+        end
+    end
 end
 
 return M
