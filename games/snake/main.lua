27dda9dd6d126ea9c890d9752d1a1545744cd6e6
Jake Vandereay <gvandereay@gmail.com>
2016-10-23 12:15:03 -0700

    update icons

--- games/snake/main.lua
+++ games/snake/main.lua
@@ -8,17 +8,19 @@ function Snake:init(args)
     local args = args or {} --lua weirdness. (make calling without args work with named arg setup)
     self.map = M.map
     self.length = args.length or 5
-    self.vel = args.vel or {x=0,y=-1}
+    self.vel = args.vel or {x=0,y=0}
     self.body = {}--NOTE keep body vals whole numbers
     self.color = args.color or M.PXL.colors.blue[2]
-    self.speed = args.speed or 5
+    self.headColor = args.headColor or M.PXL.colors.orange[3]
+    self.speed = args.speed or 6
     self.pos = args.pos
     if not pos then
         self.pos = {x=math.random(1, self.map.x),y= math.random(1,self.map.y)}
     end
-    for i=1, self.length do --init body at pos (down to up)
-        self.body[i] = {x=self.pos.x, y=self.pos.y+i-1}
-    end
+--     for i=1, self.length do --init body at pos (down to up)
+--         self.body[i] = {x=self.pos.x, y=self.pos.y+i-1}
+--     end
+    self:rebuildBody()
 end
 
 function Snake:tick(dt) --update function
@@ -28,20 +30,36 @@ function Snake:tick(dt) --update function
     if M.PXL.round(self.pos.x) ~= M.PXL.round(oldpos.x) or M.PXL.round(self.pos.y) ~= M.PXL.round(oldpos.y) then
         self:rebuildBody()
     end
+    local head = self.body[1]
+    --check for pellet
+    if M.map[head.x][head.y] == 1 then
+        self.length = self.length+1 --Yum!
+        M.map[head.x][head.y] = 0
+    end
 end
 
 function Snake:rebuildBody()
     local new = {}
     new[1] = {x=M.PXL.round(self.pos.x), y=M.PXL.round(self.pos.y)}
-    for i=1, self.length-1 do--loop through all but the last shifting the body
-        new[i+1] = self.body[i]
+    for i=1, math.min(#self.body+1,self.length-1) do--loop through all but the last shifting the body
+        new[i+1] = self.body[i] or new[i]
     end
     self.body = new
 end
 
 function Snake:draw(offset)
-    love.graphics.setColor(self.color)
-    for _,seg in ipairs(self.body) do
+    local function fadecolor(color, x)
+       local c = {} --python made me forget if this needs to be done
+       for i, v in ipairs(color) do
+           c[i] = v*math.exp(1)^(-x/85)
+       end
+       return c
+    end
+--     love.graphics.setColor(self.color)
+    for i = #self.body, 1, -1 do
+        local seg = self.body[i]
+        love.graphics.setColor(fadecolor(self.color, i))
+        if i == 1 then love.graphics.setColor(self.headColor) end --draw head a different color
         love.graphics.points(seg.x+0.5+offset.x, seg.y+0.5+offset.y)
     end
 end
